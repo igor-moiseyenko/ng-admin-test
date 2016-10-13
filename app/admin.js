7bb46b5a1dae219c4573d91f289b9a0b242b2006
Igor <Igor@Igors-MacBook-Pro.local>
2016-10-13 03:12:14 +0300

    Relationships and field types.

--- app/admin.js
+++ app/admin.js
@@ -23,14 +23,39 @@
         // Add the user entity to the admin application
         admin.addEntity(user);
 
+        // Create a comments entity.
+        var comments = nga.entity('comments');
+        admin.addEntity(comments);
+
         // Create a post entity. Post API endpoint will be http://jsonplaceholder.typicode.com/posts/:id
         var post = nga.entity('posts');
 
-        // Set the fields of the post entity list view
         post.listView().fields([
           nga.field('id'),
           nga.field('title'),
-          nga.field('userId')
+          nga.field('userId', 'reference')
+            .targetEntity(user)
+            .targetField(nga.field('username'))
+            .label('User'),
+        ]);
+
+        // Set the fields of the post entity list view
+        post.showView().fields([
+          nga.field('title'),
+          nga.field('body', 'text'),
+          nga.field('userId', 'reference')
+            .targetEntity(user)
+            .targetField(nga.field('username'))
+            .label('User'),
+          nga.field('comments', 'referenced_list')
+            .targetEntity(comments)
+            .targetReferenceField('postId')
+            .targetFields([
+              nga.field('email'),
+              nga.field('name')
+            ])
+            .sortField('id')
+            .sortDir('DESC')
         ]);
 
         // Add the post entity to the admin application
